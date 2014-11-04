0c8fd91b39a586cfc29e32e5d6761eb33189237e
Masaya Kazama <miyan@t-p.jp>
2014-11-04 21:16:56 +0900

    Initial commit

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('hierarchyApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
